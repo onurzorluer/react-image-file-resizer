146ad4f91610abdd7e3b0dcc64ea1209356c2cc6
Onur Zorluer <onurzorluer1@gmail.com>
2018-12-13 15:50:05 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+/** 
+*
+* @author Onur Zorluer.
+*
+*/
+import React, { Component } from 'react';
+
+class Resizer extends Component {
+
+    static changeHeightWidth(height, maxHeight, width, maxWidth) {
+        if (width > maxWidth) {
+            height = Math.round(height * maxWidth / width);
+            width = maxWidth;
+        }
+        if (height > maxHeight) {
+            width = Math.round(width * maxHeight / height);
+            height = maxHeight;
+        }
+        return {height, width}
+    }
+
+    static resizeAndRotateImage(image, maxHeight, maxWidth, compressFormat = "jpeg" , quality = 100, rotation = 0) {
+        var qualityDecimal = quality / 100;
+        var canvas = document.createElement('canvas');
+    
+        var width = image.width;
+        var height = image.height;
+    
+        canvas.width = this.changeHeightWidth(height, maxHeight, width, maxWidth).width;
+        canvas.height = this.changeHeightWidth(height, maxHeight, width, maxWidth).height;
+
+        width = canvas.width;
+        height = canvas.height;
+
+        var ctx = canvas.getContext("2d");
+
+        if(rotation) {
+        ctx.rotate(rotation * Math.PI / 180);
+        if (rotation === 90) {
+            ctx.translate(0, -canvas.width);
+        } else if (rotation === 180) {
+            ctx.translate(-canvas.width, -canvas.height);
+        } else if (rotation === 270) {
+            ctx.translate(-canvas.height, 0);
+        } else if (rotation === 0 || rotation === 360) {
+            ctx.translate(0, 0);
+        } else { return "Rotation Error"}
+        } 
+        ctx.drawImage(image, 0, 0, width, height); 
+    
+        return canvas.toDataURL(`image/${compressFormat}`, qualityDecimal);
+    }
+
+    static createResizedImage(file, maxWidth, maxHeight, compressFormat, quality, rotation, responseUri) {
+        const reader = new FileReader();
+        if(file) {
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                var image = new Image();
+                image.src = reader.result;
+                image.onload = function () {
+                var resizedDataUrl = Resizer.resizeAndRotateImage(image, maxWidth, maxHeight, compressFormat, quality, rotation);
+                responseUri(resizedDataUrl)
+                };        
+            };
+            reader.onerror = error => {
+            responseUri(error)
+            };
+        } else {callBack('File Not Found')}
+    }
+}
+    
+    export default { imageFileResizer: (file, maxWidth, maxHeight, compressFormat, quality, rotation, responseUri) => {
+    return Resizer.createResizedImage(file, maxWidth, maxHeight, compressFormat, quality, rotation, responseUri)
+    } }
+
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+
+var path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'commonjs react' 
+  }
+};
